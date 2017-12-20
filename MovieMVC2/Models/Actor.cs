60bce379332e8e8bed8dac01f05ba558f564d32c
Martin Larsson <martin@mlarsson.se>
2017-12-20 07:49:11 +0100

    removed range from date time

--- MovieMVC2/Models/Actor.cs
+++ MovieMVC2/Models/Actor.cs
@@ -13,7 +13,6 @@ namespace MovieMVC2.Models
         [StringLength(100, ErrorMessage = "Name cannot be more than 100 characters")]
         public string Name { get; set; }
         [Required(ErrorMessage = "You must enter a birth date")]
-        [Range(1900, 2017, ErrorMessage = "You must enter a valid date")]
         public DateTime BirthDate { get; set; }
         public string Bio { get; set; }
         public virtual ICollection<Role> Roles { get; set; }
